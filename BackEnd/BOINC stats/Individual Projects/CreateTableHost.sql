5eb859cb26c8d23fbdd6f9ebce67979f44a4cb32
grcgrc <grctestemail@gmail.com>
2016-04-29 16:45:09 +0100

    SQL

---
+++ BackEnd/BOINC stats/Individual Projects/CreateTableHost.sql
@@ -0,0 +1,65 @@
+/* ATLAS
+<host>
+  <id>2</id>
+  <userid>2</userid>
+  <total_credit>22167.671988</total_credit> YEP
+  <expavg_credit>0.096904</expavg_credit> YEP
+  <expavg_time>1437731111.094922</expavg_time> YEP
+  <p_vendor>GenuineIntel</p_vendor> YEP
+  <p_model>Intel(R) Core(TM) i7-2677M CPU @ 1.80GHz [x86 Family 6 Model 42 Stepping 7]</p_model>
+  <os_name>Darwin</os_name>
+  <os_version>14.5.0</os_version>
+  <coprocs>[BOINC|7.4.42][vbox|4.3.20r96996]</coprocs>
+
+  <create_time>1401094597</create_time> YEP
+  <rpc_time>1444833336</rpc_time> YEP
+  <timezone>7200</timezone> YEP
+  <ncpus>4</ncpus> YEP
+  <p_fpops>2510519606.477564</p_fpops> YEP
+  <p_iops>6703348472.993946</p_iops> YEP
+  <p_membw>1000000000.000000</p_membw> YEP
+  <m_nbytes>4294967296.000000</m_nbytes> YEP
+  <m_cache>1000000.000000</m_cache> YEP
+  <m_swap>12620140544.000000</m_swap> YEP
+  <d_total>249769230336.000000</d_total> YEP
+  <d_free>11164897280.000000</d_free> YEP
+  <n_bwup>120371.817554</n_bwup> YEP
+  <n_bwdown>5081652.184258</n_bwdown> YEP
+  <avg_turnaround>41398.057299</avg_turnaround> YEP
+  <credit_per_cpu_sec>0.000000</credit_per_cpu_sec> YEP
+  <host_cpid>1aec0d53f97802118accae88b7556e42</host_cpid> YEP
+</host>
+*/
+create table host (
+    id                      integer         not null, yep
+    userid                  integer         not null, yep
+    total_credit            double          not null,
+    expavg_credit           double          not null,
+    expavg_time             double          not null,
+    p_vendor                varchar(254),
+    p_model                 varchar(254),
+    os_name                 varchar(254),
+    os_version              varchar(254),
+    coprocs                 varchar(254);
+
+    create_time             integer         not null, yep
+    rpc_time                integer         not null,
+    timezone                integer         not null,
+    p_ncpus                 integer         not null,
+    
+    p_fpops                 double          not null,
+    p_iops                  double          not null,
+    p_membw                 double          not null,
+    m_nbytes                double          not null,
+    m_cache                 double          not null,
+    m_swap                  double          not null,
+
+    d_total                 double          not null,
+    d_free                  double          not null,
+    n_bwup                  double          not null,
+    n_bwdown                double          not null,
+    avg_turnaround          double          not null,
+    credit_per_cpu_sec      double          not null,
+    host_cpid               varchar(254),
+    primary key (id)
+) engine=InnoDB;
