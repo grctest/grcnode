efc60e93ac3ea54005d578b890a60ca575a6f1f6
grctest <grctestemail@gmail.com>
2016-10-16 00:03:19 +0100

    Update GridcoinAutoNode.sh

--- BackEnd/Nodes/AutoNode/GridcoinAutoNode.sh
+++ BackEnd/Nodes/AutoNode/GridcoinAutoNode.sh
@@ -38,24 +38,14 @@ echo "addnode=typh00n.net" >> $config
 echo "addnode=node.gridcoin.us" >> $config
 echo "addnode=gridcoin.asia" >> $config
 echo "addnode=frankfurt.grcnode.co.uk" >> $config
-echo "addnode=sf.grcnode.co.uk" >> $config
-echo "addnode=nyc.grcnode.co.uk" >> $config
-echo "addnode=japan.grcnode.co.uk" >> $config
 echo "addnode=quebec.gridcoin.co.il" >> $config
 echo "addnode=ils.gridcoin.co.il" >> $config
 echo "addnode=node.gridcoinapp.xyz" >> $config
-# Maximum connection entries broke incoming connections!
-#echo "maxoutboundconnections=250" >> $config
-#echo "maxinboundconnections=500" >> $config
 echo "rpcport=9332" >> $config
 randUser=`< /dev/urandom tr -dc A-Za-z0-9 | head -c128`
 randPass=`< /dev/urandom tr -dc A-Za-z0-9 | head -c128`
 echo "rpcuser=$randUser" >> $config
 echo "rpcpassword=$randPass" >> $config
-
-# set prune amount to size of `/` 60% (and then by /1000 to turn KB to MB) => /1666
-# Gridcoin does not support pruning yet.
-#echo "prune="$(expr $(df | grep '/$' | tr -s ' ' | cut -d ' ' -f 2) / 1666) >> $config # safe enough for now
 
 # setting up ntp
 echo "########### Setting up NTP"
@@ -69,19 +59,18 @@ apt-get -y install ntp
 #ufw allow 22/tcp
 #ufw --force enable
 
-echo "########### Setting up autostart (cron)"
-crontab -l > tempcron
-echo "0 3 * * * reboot" >> tempcron  # reboot at 3am to keep things working okay
-crontab tempcron
-rm tempcron
+#echo "########### Setting up autostart (cron)"
+#crontab -l > tempcron
+#echo "0 3 * * * reboot" >> tempcron  # reboot at 3am to keep things working okay
+#crontab tempcron
+#rm tempcron
 
-# only way I've been able to get it reliably to start on boot
-# (didn't want to use a service with systemd so it could be used with older ubuntu versions, but systemd is preferred)
-sed -i '2a\
-sudo -u gridcoin /usr/local/bin/gridcoinresearchd' /etc/rc.local
+# Attempting to start GRC on boot
+#sed -i '2a\
+#sudo -u gridcoin /usr/local/bin/gridcoinresearchd' /etc/rc.local
 
 #echo "############ Add an alias for easy use"
-#echo "alias grc=\"sudo -u gridcoin gridcoinresearchd -datadir=/home/gridcoin/.GridcoinResearch\"" >> ~/.bashrc  # example use: btc getinfo
+echo "alias grc=\"sudo -u gridcoin gridcoinresearchd -datadir=/home/gridcoin/.GridcoinResearch\"" >> ~/.bashrc  # example use: btc getinfo
 
 ## Adding the following function to ~/.bashrc allows for 'grc' to be used in bash scripts! Asliases don't work in bash scripts.
 #function grc()
@@ -89,7 +78,5 @@ sudo -u gridcoin /usr/local/bin/gridcoinresearchd' /etc/rc.local
 # sudo -u gridcoin gridcoinresearchd -datadir=/home/gridcoin/.GridcoinResearch "$@"
 #}
 #export -f grc
-
-
 
 reboot
