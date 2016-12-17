67f37ac8e0acd824e44169093a3f644fbfb88287
Peppernrino <Peppernrino@users.noreply.github.com>
2016-12-16 19:53:59 -0600

    Update GridcoinAutoNode.sh

--- BackEnd/Nodes/AutoNode/GridcoinAutoNode.sh
+++ BackEnd/Nodes/AutoNode/GridcoinAutoNode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "########### The server will reboot when the script is complete"
-echo "########### Changing to /home/ dir"
+echo "########### Changing to home dir"
 cd ~
 
 echo "########### Adding Google DNS"
